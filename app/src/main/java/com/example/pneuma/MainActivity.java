a1ec8c505ad8ef41ae16e4bced4d2c499054fa91
mohammed <mohammedabdk95@gmail.com>
2021-01-14 23:24:21 -0700

    adding Toggle Hamburger button

--- app/src/main/java/com/example/pneuma/MainActivity.java
+++ app/src/main/java/com/example/pneuma/MainActivity.java
@@ -1,14 +1,105 @@
 package com.example.pneuma;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import androidx.appcompat.widget.Toolbar;
+import android.widget.Toast;
+//import android.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private RecyclerView postList;
+    private Toolbar mToolbar;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //add the toolbar to the main activity hamburger
+        mToolbar = (Toolbar)findViewById(R.id.main_page_toolbar);
+        //setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Home");
+
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawble_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(MainActivity.this, drawerLayout,R.string.drawer_open, R.string.drawer_closer);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        navigationView = (NavigationView)findViewById(R.id.navigation_view);
+        View navView = navigationView.inflateHeaderView(R.layout.navigation_header); //storing the navigation_header page in the layout in navView
+
+
+        //hamburger bar function
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                UserMenuSelector(item);
+                return false;
+            }
+        });
+    }
+
+
+    //sandwich
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    //this fucntion will allow the user to navigate through home, profile ...after clicking on it
+    private void UserMenuSelector(MenuItem item) {
+        switch (item.getItemId()){
+
+            case R.id.nav_profile:
+                Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_home:
+                Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_friends:
+                Toast.makeText(this, "Friends List", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_add_friends:
+                Toast.makeText(this, "Add Friend", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_chat:
+                Toast.makeText(this, "Chat", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_settings:
+                Toast.makeText(this, "Setting", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_logout:
+                Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_personality:
+                Toast.makeText(this, "Personality", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
